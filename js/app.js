acf18111f6e6de657e548c97b707a3d14d96178c
Juliette <julietteredheadgirl@yahoo.com>
2016-03-30 17:11:32 -0500

    Delete bad jQuery

--- js/app.js
+++ js/app.js
@@ -125,8 +125,8 @@ $('#rightArrow').click(function(){
   var $imageNext = $activeImg.parent().next().children().attr("href");//get parent of current img, which is <li>, then get next <li>, then get child href
   //var $captionNext = $captionText.parent().next().children().attr("alt");
   $image.attr("src", $imageNext);//change location of "src" to be equal to next image
-  $captionNext = $captionText.next();
-  $caption.text("alt", $captionNext);//get caption text of prev image
+  $captionNext = $captionText;
+  $caption.text($captionNext);//get caption text of prev image
   //$activeImg = $activeImg.next();//get next image
   console.log(imageNext);
   console.log(activeImg);
@@ -137,10 +137,10 @@ $('#leftArrow').click(function(){
     //.prev() can only select elements, no attributes
   var $activeImg = $(".selected"); //current img w/selected class
   var $imagePrev = $activeImg.parent().prev().children().attr("href");//get parent of current img, which is <li>, then get prev <li>, then get child href
-  var $captionText =$(".selected").children("img").attr("alt");
+  var $captionText = $(".selected").children("img").attr("alt");
   $image.attr("src", $imagePrev);//change location of "src" to be equal to previous image
   //$captionText = $activeImg.parent().prev().children("img").attr("alt");//get parent of current img, which is <li>, then get prev <li>, then get child image attribute/alt text
-  $captionPrev = $captionText.prev();
+  $captionPrev = $captionText;
   $caption.text($captionText);//get caption text of prev image
   //$activePhoto = $activeImg.prev();//get previous image
   console.log(imagePrev);
